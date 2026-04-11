993b3982edc35fd3defb92d360c1694929bc56ea
CodeWithMihran <sohail.mihran@gmail.com>
2026-04-11 11:06:53 +0530

    Practiced Nested Routes in Next.Js

---
+++ next02_routing/app/product/page.js
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <div className='flex justify-center gap-5 py-3'>
+            <Link href='/product/men'>Men's Collection</Link>
+            <Link href='/product/women'>Women's Collection</Link>
+        </div>
+      <h1>Product Page</h1>
+    </div>
+  )
+}
+
+export default page
